6c692962dab824134416588a813c6e5ad0a6255e
pezholio <pezholio@gmail.com>
2021-11-08 16:12:56 +0000

    Improve on validation
    This improves the exception filter to get the
    error messages in a format we can use in
    the templates. There's still improvements that
    can be made I think, but I'm learning!

--- src/blog-posts/http-exception-filter.ts
+++ src/blog-posts/http-exception-filter.ts
@@ -1,21 +1,33 @@
 
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { validate } from 'class-validator';
+import { CreateBlogPostDto } from './interfaces/create-blog-post.dto'
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  async catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
-    const exceptionResponse = exception.getResponse()
-    const errors = exceptionResponse["message"].map(message => {
-      return {
-        text: message,
-        href: "#"
-      }
+
+    const blogPost = Object.assign(new CreateBlogPostDto(), request.body);
+    const validationErrors = await validate(blogPost);
+    const errors = {};
+    const errorMessages = [];
+
+    validationErrors.forEach(error => {
+      errors[error.property] = { text: Object.values(error.constraints).join() }
+      errorMessages.push({
+        text: Object.values(error.constraints).join(),
+        href: `#${error.property}`
+      })
     })
 
-    response.render('blog-posts/new', { errors: errors })
-
+    response.render('blog-posts/new', {
+      errors: errors,
+      errorMessages: errorMessages,
+      blogPost: blogPost
+    })
   }
 }
